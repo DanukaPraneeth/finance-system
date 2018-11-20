85e70380df675eb0db5752529e14e5c21e6914b1
tharinduwso2telco <tharindu@wso2telco.com>
2018-11-21 01:46:22 +0530

    Setup models for each bills

---
+++ src/main/java/com/backend/core/roles/StaffRole.java
@@ -0,0 +1,28 @@
+package com.backend.core.roles;
+
+public class StaffRole {
+
+    private int roleId;
+    private String roleName;
+
+    public StaffRole(int roleId, String roleName) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
